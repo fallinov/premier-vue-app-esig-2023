0e720629ce2d4e2261f1c1d3214728f8cb245a88
Steve Fallet <steve.fallet@divtec.ch>
2023-05-17 11:39:45 +0200

    test

---
+++ src/views/ListeDestinationsView.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import CarteDestination from "@/components/CarteDestination.vue";
+import MonBouton from "@/components/MonBouton.vue";
+</script>
+
+<template>
+    <h1>{{ titre }}</h1>
+    <mon-bouton couleur="red" texte="toto">Txtx</mon-bouton>
+    <mon-bouton couleur="yellow">Titi</mon-bouton>
+    <mon-bouton texte="Bonjour">Tutu</mon-bouton>
+    <mon-bouton></mon-bouton>
+    <MonBouton>blibla</MonBouton>
+    <img class="logo" src="/favicon.ico" alt="Icone">
+    <img class="logo" src="@/assets/logo.svg" alt="Logo">
+    <div class="nos-destinations">
+        <carte-destination
+                v-for="destFor in destinations"
+                :key="destFor.id"
+                :dest="destFor"
+        ></carte-destination>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
